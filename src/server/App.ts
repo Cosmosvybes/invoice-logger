548e7e01f6525b1c6fa4fb3c04f9f2c0dbfc8e94
cosmos <alfredchrisayo@gmail.com>
2024-08-15 11:38:30 +0000

    server

--- src/server/App.ts
+++ src/server/App.ts
@@ -1,24 +1,24 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  onSnapshot,
-  addDoc,
-  getDocs,
-  collection,
-} from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import {
+//   getFirestore,
+//   onSnapshot,
+//   addDoc,
+//   getDocs,
+//   collection,
+// } from "firebase/firestore";
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBjFopa9GEVKbRQlMSNZ6WTO6Sr4g155UY",
-  authDomain: "etherbill-4e92b.firebaseapp.com",
-  projectId: "etherbill-4e92b",
-  storageBucket: "etherbill-4e92b.appspot.com",
-  messagingSenderId: "359385798596",
-  appId: "1:359385798596:web:3c992ccfd2b9190746b691",
-  measurementId: "G-NCV1NSZJX1",
-};
-initializeApp(firebaseConfig);
-const db = getFirestore();
-const colRef = collection(db, "users");
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBjFopa9GEVKbRQlMSNZ6WTO6Sr4g155UY",
+//   authDomain: "etherbill-4e92b.firebaseapp.com",
+//   projectId: "etherbill-4e92b",
+//   storageBucket: "etherbill-4e92b.appspot.com",
+//   messagingSenderId: "359385798596",
+//   appId: "1:359385798596:web:3c992ccfd2b9190746b691",
+//   measurementId: "G-NCV1NSZJX1",
+// };
+// initializeApp(firebaseConfig);
+// const db = getFirestore();
+// const colRef = collection(db, "users");
 
-// export function 
+// // export function 
