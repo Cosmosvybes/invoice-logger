f9cd67229472e0addf360e1e3d7adf08d7c6764e
cosmosayo <alfredchrisayo@gmail.com>
2025-12-28 06:22:29 +0100

    prod

--- src/Components/constants/Index.ts
+++ src/Components/constants/Index.ts
@@ -1,3 +1,3 @@
 export const ownerAddress = 0x62b0d48d3ca8be2233367d6f9269216c6f898234;
-// export const API_URL = "https://ether-bill-server-1.onrender.com";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://ether-bill-server-1.onrender.com";
+// export const API_URL = "http://localhost:8080";
